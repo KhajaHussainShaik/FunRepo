b2e71e6d39f6e0893a731751fe520f184f8a3ce9
KH <kh@gmail.com>
2016-10-15 19:45:38 -0400

    Adding new FunDB Project Solution

---
+++ FunDBSolution/FunDBProject/dbo/Tables/Book.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Book] (
+    [BookID]        INT          NOT NULL,
+    [BookName]      VARCHAR (50) NULL,
+    [BookPrice]     NUMERIC (18) NOT NULL,
+    [Author]        VARCHAR (50) NULL,
+    [PublishedDate] DATE         NULL
+);
+
---
+++ FunDBSolution/FunDBProject/dbo/Tables/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserID]       INT          NOT NULL,
+    [UserName]     VARCHAR (50) NULL,
+    [UserCity]     VARCHAR (50) NULL,
+    [AccounNumber] VARCHAR (50) NOT NULL
+);
+
